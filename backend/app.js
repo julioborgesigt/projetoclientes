131279d22cc61997b997a5564a898f22ee8e6f66
julioborgesigt <julio.aparecido3@email.com>
2024-12-01 13:42:42 -0300

    json

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path'); // Importação do módulo path
 
 dotenv.config();
 
@@ -14,10 +15,13 @@ app.use(bodyParser.json());
 app.use('/auth', authRoutes);
 
 
+// Configura o uso de arquivos estáticos (CSS, JS, etc.) a partir da pasta frontend
+app.use(express.static(path.join(__dirname, '../frontend')));
+
 // Rota para a página principal
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'index.html')); // Supondo que você tenha um index.html na pasta public
-  });
+  res.sendFile(path.join(__dirname, '../frontend', 'index.html')); // Caminho para o index.html
+});
   
 
 const PORT = process.env.PORT || 3000;
