3ba9fb1cfca370179b8685e9787f360cccf734a4
julioborgesigt <julio.aparecido3@email.com>
2024-12-01 14:37:03 -0300

    json

--- banco.sql
+++ banco.sql
@@ -1,11 +1,6 @@
 USE zjbjbi_banco;
 
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
+
 
 CREATE TABLE clientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
