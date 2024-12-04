bad1f292395982ef9e5d2c4b33138f4913d5c881
julioborgesigt <julio.aparecido3@email.com>
2024-12-04 12:39:44 -0300

    json

--- frontend/script.js
+++ frontend/script.js
@@ -132,7 +132,16 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-
+// Função para buscar a lista de clientes
+async function getClients() {
+    try {
+        const response = await fetch('/clientes/list');
+        const data = await response.json();
+        displayClients(data);
+    } catch (error) {
+        console.error('Erro ao buscar clientes:', error);
+    }
+}
 
 
     // Carregar a mensagem padrão ao abrir o dashboard
